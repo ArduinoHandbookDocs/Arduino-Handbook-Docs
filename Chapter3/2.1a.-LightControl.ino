dc4f14945c1c7221fcf88033fc214ccea87119a9
Cong Nguyen Ba <congngba.21@gmail.com>
2022-02-20 01:35:28 +0700

    Create 2.1a.-LightControl.ino

---
+++ Chapter3/2.1a.-LightControl.ino
@@ -0,0 +1,12 @@
+int ledPin= 13;
+void setup(){
+pinMode(ledPin, OUTPUT); //pin13. OUTPUT
+}
+void loop(){
+digitalWrite(ledPin,HIGH); // output HIGH level
+signal to ledPin pin.
+delay(1000); //stop program 1000s(1 second)
+digitalWrite(ledPin, LOW); // output a LOW signal
+for ledPin pin
+delay(1000); //stop program 1000s(1 second)
+}
