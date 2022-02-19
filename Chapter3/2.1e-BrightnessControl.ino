712f7cea0d7ec7a10e15421bb157675aa8baf56c
Cong Nguyen Ba <congngba.21@gmail.com>
2022-02-20 01:44:44 +0700

    Create 2.1e-BrightnessControl.ino

---
+++ Chapter3/2.1e-BrightnessControl.ino
@@ -0,0 +1,18 @@
+// declare 3 LEDs at 3 pins digital signal with the symbol PWM ~
+int led1 = 3; //declare led1 on pin 3
+int led2 = 5; //declare led1 on pin 5
+int led3 = 6; //declare led1 on pin 6
+
+void setup()
+{
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+}
+
+void loop()
+{
+analogWrite(led1, 0); // led1 is not lit
+analogWrite(led2, 127); // led2 is half lit
+analogWrite(led3, 255); // led3 is on
+}
