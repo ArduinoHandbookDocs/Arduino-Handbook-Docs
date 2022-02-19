ade321368c7926ab6a133cf83d2e59788d132d90
Cong Nguyen Ba <congngba.21@gmail.com>
2022-02-20 01:38:13 +0700

    Update 2.1b-LEDs.ino

--- Chapter3/2.1b-LEDs.ino
+++ Chapter3/2.1b-LEDs.ino
@@ -3,15 +3,15 @@ int yellowLed = 12; //yellow light
 int redLed = 13; // red light
 
 void setup() {
-pinMode(greenLed, OUTPUT);
-pinMode(yellowLed, OUTPUT);
-pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
 }
 
 void loop() { //Open the lights one by one
-digitalWrite(greenLed, HIGH);
-delay(2000);
-digitalWrite(yellowLed, HIGH);
-delay(2000);
-digitalWrite(redLed, HIGH);
+  digitalWrite(greenLed, HIGH);
+  delay(2000);
+  digitalWrite(yellowLed, HIGH);
+  delay(2000);
+  digitalWrite(redLed, HIGH);
 }
