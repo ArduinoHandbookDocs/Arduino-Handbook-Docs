5551705a13258e8a8539c5a64d38bf00015d9b0a
Cong Nguyen Ba <congngba.21@gmail.com>
2022-02-20 01:37:19 +0700

    Create 2.1b-LEDs.ino

---
+++ Chapter3/2.1b-LEDs.ino
@@ -0,0 +1,17 @@
+int greenLed = 11; //green light
+int yellowLed = 12; //yellow light
+int redLed = 13; // red light
+
+void setup() {
+pinMode(greenLed, OUTPUT);
+pinMode(yellowLed, OUTPUT);
+pinMode(redLed, OUTPUT);
+}
+
+void loop() { //Open the lights one by one
+digitalWrite(greenLed, HIGH);
+delay(2000);
+digitalWrite(yellowLed, HIGH);
+delay(2000);
+digitalWrite(redLed, HIGH);
+}
