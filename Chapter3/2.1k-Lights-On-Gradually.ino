bc400c163d6ffbcdddfa4faa01da368d5f4027d9
Cong Nguyen Ba <congngba.21@gmail.com>
2022-02-20 01:55:38 +0700

    Create 2.1k-Lights-On-Gradually.ino

---
+++ Chapter3/2.1k-Lights-On-Gradually.ino
@@ -0,0 +1,13 @@
+int led1 = 13;
+unsigned long time; //variable to store the time value for millis()
+
+void setup()
+{
+pinMode(led1, OUTPUT);
+}
+void loop()
+{
+time = millis(); // assign time value to time variable
+analogWrite(led, time/100);
+// control the brightness of the LED based on the value of the time variable
+}
