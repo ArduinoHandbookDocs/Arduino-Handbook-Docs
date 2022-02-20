f3764d6ad7e8a6e2fe5bb4e2192ec6d0077ad033
Cong Nguyen Ba <congngba.21@gmail.com>
2022-02-20 19:56:34 +0700

    Update 2.1f-BuzzerControl.ino

--- English/Chapter3/2.1f-BuzzerControl.ino
+++ English/Chapter3/2.1f-BuzzerControl.ino
@@ -1,15 +1,15 @@
 /* Example 2.1f: Using analogWrite() to 
 control how loud or quiet the buzzer sounds */
 
-int photo= A1; //specifies the analog pin A1 to read the photoresistor
-int photoValue; //initialize the variable to read the photoresistor value
+int photo= A1; // specifies the analog pin A1 to read the photoresistor
+int photoValue; // initialize the variable to read the photoresistor value
 int led= 6; // pin 6 can use analogWrite() function
 
 void setup(){
-pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
-photoValue = analogRead(photo); // read analog signal, get photoresistance value
-analogWrite(led, photoValue/4); // turn on the light at pin 6 based on the light intensity read from the photoresistor
+  photoValue = analogRead(photo); // read analog signal, get photoresistance value
+  analogWrite(led, photoValue/4); // turn on the light at pin 6 based on the light intensity read from the photoresistor
 }
