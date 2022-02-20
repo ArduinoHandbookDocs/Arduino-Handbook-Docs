7b28b0c038dfd4858076ba6914c4895a04831d5a
Cong Nguyen Ba <congngba.21@gmail.com>
2022-02-20 19:52:36 +0700

    Update 2.1d-1-Light-Button.ino

--- English/Chapter3/2.1d-1-Light-Button.ino
+++ English/Chapter3/2.1d-1-Light-Button.ino
@@ -1,3 +1,8 @@
+/*
+Example 2.1d: Read the signal from the push button and then turn on-off the corresponding light
+Method 1: use pinMode(INPUT) to read the value from the push button.
+*/
+
 int button = 10; //button connected to pin 10
 boolean buttonState = false; //set the button state test variable to 0, equivalent to the state that the button has not been pressed
 int led = 13;
