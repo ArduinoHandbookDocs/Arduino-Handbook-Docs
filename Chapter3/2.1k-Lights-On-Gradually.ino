045086afa22f5183d121b36f0f7f965c6f8f3d61
Cong Nguyen Ba <congngba.21@gmail.com>
2022-02-20 19:25:24 +0700

    Update 2.1k-Lights-On-Gradually.ino

--- Chapter3/2.1k-Lights-On-Gradually.ino
+++ Chapter3/2.1k-Lights-On-Gradually.ino
@@ -1,13 +1,16 @@
+/* Example 2.1k: Turn lights on gradually using millis() function */
+
 int led1 = 13;
 unsigned long time; //variable to store the time value for millis()
 
 void setup()
 {
-pinMode(led1, OUTPUT);
+  pinMode(led1, OUTPUT);
 }
+
 void loop()
 {
 time = millis(); // assign time value to time variable
-analogWrite(led, time/100);
+analogWrite(led, time / 100);
 // control the brightness of the LED based on the value of the time variable
 }
