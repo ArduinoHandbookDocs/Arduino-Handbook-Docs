dc286eea6b576ca1f6862096c5055d8886ca934f
Cong Nguyen Ba <congngba.21@gmail.com>
2022-02-20 19:18:05 +0700

    Update 2.1h-Rheostat-LED-Brightness.ino

--- Chapter3/2.1h-Rheostat-LED-Brightness.ino
+++ Chapter3/2.1h-Rheostat-LED-Brightness.ino
@@ -1,10 +1,13 @@
-int poten= A2; //Specify analog pin A2 to read rheostat
-int led= 6;
-void setup(){
-pinMode(led, OUTPUT);
+/* Example 2.1h: read the value of the rheostat 
+to control the brightness of the LED */
+
+int poten = A2; //Specify analog pin A2 to read rheostat
+int led = 6;
+
+void setup() {
+  pinMode(led, OUTPUT);
 }
 void loop() {
-int potenValue = analogRead(poten); // read analog
-signal, get rheostat value
-analogWrite(led, potenValue);
+  int potenValue = analogRead(poten); // read analog signal, get rheostat value
+  analogWrite(led, potenValue);
 }
